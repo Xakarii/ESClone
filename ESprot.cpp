a488903c78e193ff79844924579ecd53f9ded46e
E.M.Mondragon <xakarii@yahoo.com>
2022-02-23 10:58:19 -0600

    added io for gettting symptoms and outputting a diagnosis

--- ESprot.cpp
+++ ESprot.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <list>
+#include <stack>
 
 using namespace std;
 
@@ -41,7 +42,7 @@ string weightGain, lossOfLO, depressGreaterTwoYrs;
 vector <string> variableList[10];
 vector <string> conclusionList[10]; 
 vector <string> clauseVarList[10];
-vector <int> clauseStack = {10, 0};
+stack <int> clauseStack;
 vector <int> statementStack; 
 vector <int> instantiatedList;
 vector <string> buffer;
@@ -54,17 +55,195 @@ void getClauseLocation(string clause);
 
 main() 
 {
-	cout << "Any symptoms?";
 	int conclusionStatementNum = 0;
-	string symptom;
-	string depression;
-	cin >> symptom;
-	statementStack.push_back(1);
-	cout << "Depression?";
-	cin >> depression;
-	statementStack.push_back(2);
+	string symptom = "";
+	string depression = "";
+	string weightGain = "";
+	string lossOfLovedOne = "";
+	string depressionGreater2yrs = "";
+	string difficultyConcentrating = "";
+	string hyper = "";
+	string binging = "";
+	string suicidal = "";
+	string isolation = "";
+	string irregSleep = "";
+	string anxiety = "";
+	string panicAttack = "";
+	string anxietyGreater2yrs = "";
+	string hallucinations = "";
+	string manicEpisodes = "";
+	string fatigue = "";
+	string helplessness = "";
+	string insomnia = "";
+	string missingTime = "";
+	string extremeDebil = "";
+	string avoidingPeople = "";
 	
-	determine_member_concl_list(conclusionStatementNum);
+	
+	cout << "Any symptoms? Enter y/n";
+	cin >> symptom;
+	if (symptom == "y") {
+		cout << "Depression?";
+		cin >> depression;	
+		if (depression == "y") {
+			cout << "Weight gain?";
+			cin >> weightGain;	
+			if (weightGain == "y") {
+				cout << "Loss of Loved one?";
+				cin >> lossOfLovedOne;
+				if (lossOfLovedOne == "y") {
+					cout << "Depression longer than 2 years?";
+					cin >> depressionGreater2yrs;
+					if (depressionGreater2yrs == "y") {
+						cout << "D = Disthymia";
+					}
+					else if (depressionGreater2yrs == "n") {
+						cout << "D = Normal Bereavement"
+					}
+				}
+				else if (lossOfLovedOne == "n") {
+					cout << "Difficulty concentrating?";
+					cin >> difficultyConcentrating;
+					if (difficultyConcentrating == "y") {
+						cout << "Hyperactivity?";
+						cin >> hyper;
+						if (hyper == "y") {
+							cout << "D = ADHD";
+						}
+						else {
+							cout << "D = ADD";
+						}
+					}
+					else if (difficultyConcentrating == "n") {
+						cout << "Food binging?";
+						cin >> binging;
+						if (binging == "y") {
+							cout << "D = Binge eating disorder";
+						}
+						else if (binging == "n") {
+							cout << "D = Hypothyroidism";
+						}
+					}
+					
+				}
+			}
+			else if (weightGain == "n") {
+				cout << "suicidal?";
+				cin >> suicidal;	
+				if (suicidal == "y") {
+					cout << "feelings of isolation?";
+					cin >> isolation;
+					if (isolation == "y") {
+						cout << "D = MDD";
+					}
+					else if (isolation == "n") {
+						cout << "Irregular sleep patterns?";
+						cin >> irregSleep;
+						if (irregSleep == "y") {
+							cout << "D = Bipolar Disorder";
+						}
+						else if (irregSleep == "n") {
+							cout << "D = OCD";
+						}
+					}
+				}
+				
+			}		
+		}
+		else if (depression == "n") {
+			cout << "anxiety?";
+			cin >> anxiety;
+			if (anxiety == "y") {
+				cout << "Lasting more than 2 years?";
+				cin >> anxietyGreater2yrs;
+				if (anxietyGreater2yrs == "y") {
+					cout << "hallucinations?";
+					cin >> hallucinations;
+					if (hallucinations == "y") {
+						cout << "Manic episodes?";
+						cin >> manicEpisodes;
+						if (manicEpisodes == "y") {
+							cout << "D = Schizoaffective";
+						}
+						else if (manicEpisodes == "n") {
+							cout << "D = Schizophrenia";
+						}
+					}
+					else if (hallucinations == "n") {
+						cout << "Fatigue?";
+						cin >> fatigue;
+						if (fatigue == "y") {
+							cout << "Feelings of helplessness?";
+							cin >> helplessness;
+							if (helplessness == "y") {
+								cout << "D = burnout";
+							}
+							else if (helplessness == "n") {
+								cout << "D = Mental fatigue";
+							}
+						}
+						else if (fatigue == "n") {
+							cout << "Insomnia?";
+							cin >> insomnia;
+							if (insomnia == "y") {
+								cout << "D = Chronic Fatigue Syndrome";
+							}
+							else if (insomnia == "n") {
+								cout << "D = Fibromyalgia";
+							}
+						}
+						
+					}
+				}
+				else if (anxietyGreater2yrs == "n") {
+					cout << "extremeDebil?";
+					cin >> extremeDebil;
+					if (extremeDebil == "y") {
+						cout << "D = PTSD";
+					}
+					else if (extremeDebil == "n") {
+						cout << "D = GAD";
+					}
+				}
+				
+			}
+			else if (anxiety == "n") {
+				cout << "panicAttack?";
+				cin >> panicAttack;
+				if (panicAttack == "y") {
+					cout << "Avoiding people or places?";
+					cin >> avoidingPeople;
+				}
+				else if (panicAttack == "n") {
+					cout << "Missing time?";
+					cin >> missingTime;
+					if (missingTime == "y") {
+						cout << "D = DID";
+					}
+					else if (missingTime == "n") {
+						cout << "D = Hypochondria";
+					}
+				}
+				
+			}
+		}
+		
+		
+	}
+		
+	
+	/*
+	string fatigue = "";
+	string helplessness = "";
+	string insomnia = "";
+	string missingTime = "";
+	string extremeDebil = "";
+	string avoidingPeople = "";
+	*/
+	//statementStack.push_back(1);
+	//statementStack.push_back(2);
+	
+	//determine_member_concl_list(conclusionStatementNum);
 	for (const int& i : statementStack) {
     	cout << i << "  ";
     }
